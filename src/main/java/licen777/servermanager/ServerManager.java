bc611b60eba8920bf78032f35b113bfe798405ed
licen777 <licen150@bk.ru>
2022-12-04 20:10:49 +0300

    я думаю так будет лучше

--- src/main/java/licen777/servermanager/ServerManager.java
+++ src/main/java/licen777/servermanager/ServerManager.java
@@ -15,50 +15,31 @@ import com.velocitypowered.api.proxy.ProxyServer;
 )
 
 public final class ServerManager {
-
-
     private final ProxyServer proxy;
-
     @Inject
     public ServerManager(ProxyServer proxy) {
         this.proxy = proxy;
     }
-
     @Subscribe
     public void onProxyInitialize(ProxyInitializeEvent event) {
+        // /addserv command start
         CommandManager commandManager = proxy.getCommandManager();
-        // Here you can add meta for the command, as aliases and the plugin to which it belongs (RECOMMENDED)
         CommandMeta commandMeta = commandManager.metaBuilder("addserv")
-                // This will create a new alias for the command "/test"
-                // with the same arguments and functionality
                 .plugin(this)
                 .build();
 
-        // You can replace this with "new EchoCommand()" or "new TestCommand()"
-        // SimpleCommand simpleCommand = new TestCommand();
-        // RawCommand rawCommand = new EchoCommand();
-        // The registration is done in the same way, since all 3 interfaces implement "Command"
         SimpleCommand AddServer = new AddServer(proxy);
-        //BrigadierCommand commandToRegister = AddServer.createBrigadierCommand(proxy);
-
-        // Finally, you can register the command
         commandManager.register(commandMeta, AddServer);
+        // /addserv command end
 
+        // /remserv command start
         CommandMeta commandMeta1 = commandManager.metaBuilder("remserv")
-                // This will create a new alias for the command "/test"
-                // with the same arguments and functionality
+
                 .plugin(this)
                 .build();
-
-        // You can replace this with "new EchoCommand()" or "new TestCommand()"
-        // SimpleCommand simpleCommand = new TestCommand();
-        // RawCommand rawCommand = new EchoCommand();
-        // The registration is done in the same way, since all 3 interfaces implement "Command"
         SimpleCommand RemServer = new RemServer(proxy);
-        //BrigadierCommand commandToRegister = AddServer.createBrigadierCommand(proxy);
-
-        // Finally, you can register the command
         commandManager.register(commandMeta1, RemServer);
+        // /remserv command end
     }
 }
 
